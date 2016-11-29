b5989077b84b60be0b8baafc9185e3881ebfc7d4
Nex Otaku <nex@otaku.ru>
2016-11-29 09:10:14 +0300

    Версия TimestampBehavior, настроенная по умолчанию на заполнение времени из БД.

---
+++ behaviors/MysqlTimestampBehavior.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace nexotaku\toolkit\behaviors;
+
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * Версия TimestampBehavior, 
+ * настроенная по умолчанию на заполнение времени из БД.
+ *
+ * @author Nex Otaku <nex@otaku.ru>
+ */
+class MysqlTimestampBehavior extends TimestampBehavior
+{
+    public $value;
+    
+    public function init()
+    {
+        parent::init();
+        
+        if ($this->value === null) {
+            $this->value = new Expression('NOW()');
+        }
+    }
+}
